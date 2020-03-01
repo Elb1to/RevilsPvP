67af67dcddaed24924c87581ba17aac9ae5d7264
Elb1to <elbitomc@gmail.com>
2020-03-01 03:34:22 -0500

    Updated Method to get player's ping

--- src/main/java/eu/revils/revilspvp/tab/HeaderLayoutProvider.java
+++ src/main/java/eu/revils/revilspvp/tab/HeaderLayoutProvider.java
@@ -7,6 +7,7 @@ import eu.revils.revilspvp.kt.tab.TabLayout;
 import eu.revils.revilspvp.kt.util.PlayerUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 final class HeaderLayoutProvider implements BiConsumer<Player, TabLayout> {
@@ -15,13 +16,17 @@ final class HeaderLayoutProvider implements BiConsumer<Player, TabLayout> {
     public void accept(Player player, TabLayout tabLayout) {
         header: {
             tabLayout.set(0, 1, ChatColor.GRAY + "Online: " + Bukkit.getOnlinePlayers().size());
-            tabLayout.set(1, 0, "&6&lPractice");
+            tabLayout.set(1, 0, "&6&lRevils Network");
             tabLayout.set(2, 1, ChatColor.GRAY + "In Fights: " + RevilsPvP.getInstance().getCache().getFightsCount());
         }
 
         status: {
-            tabLayout.set(1, 1, ChatColor.GRAY + "Your Connection", Math.max(((PlayerUtils.getPing(player) + 5) / 10) * 10, 1));
+            tabLayout.set(1, 1, ChatColor.GRAY + "Your Connection: " + getPing(player) + "ms");
         }
+    }
+
+    public int getPing(Player player) {
+        return ((CraftPlayer) player).getHandle().ping;
     }
 
 }
