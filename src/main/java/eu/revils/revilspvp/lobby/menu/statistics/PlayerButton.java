acd8604b6ad6b3e327afc48ec80a91158cecf693
Elb1to <elbitomc@gmail.com>
2020-03-03 11:42:17 -0500

    Removed unused imports

--- src/main/java/eu/revils/revilspvp/lobby/menu/statistics/PlayerButton.java
+++ src/main/java/eu/revils/revilspvp/lobby/menu/statistics/PlayerButton.java
@@ -1,17 +1,14 @@
 package eu.revils.revilspvp.lobby.menu.statistics;
 
-import java.util.List;
-
+import com.google.common.collect.Lists;
 import eu.revils.revilspvp.RevilsPvP;
+import eu.revils.revilspvp.elo.EloHandler;
 import eu.revils.revilspvp.kittype.KitType;
+import eu.revils.revilspvp.kt.menu.Button;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-
-import com.google.common.collect.Lists;
-
-import eu.revils.revilspvp.elo.EloHandler;
-import eu.revils.revilspvp.kt.menu.Button;
+import java.util.List;
 
 public class PlayerButton extends Button {
 
