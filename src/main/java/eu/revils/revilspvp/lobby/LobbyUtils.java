e5d2bdfc811663a9dd90ca304dd24a27202319db
Elb1to <elbitomc@gmail.com>
2020-03-19 13:54:20 -0500

    Removed PlayerStats item from Hotbar

--- src/main/java/eu/revils/revilspvp/lobby/LobbyUtils.java
+++ src/main/java/eu/revils/revilspvp/lobby/LobbyUtils.java
@@ -138,7 +138,7 @@ public final class LobbyUtils {
                 inventory.setItem(1, QueueItems.JOIN_SOLO_RANKED_QUEUE_ITEM);
                 inventory.setItem(4, LobbyItems.ENABLE_SPEC_MODE_ITEM);
                 //inventory.setItem(5, EventItems.EVENTS_ITEM);
-                inventory.setItem(6, LobbyItems.PLAYER_STATISTICS);
+                //inventory.setItem(6, LobbyItems.PLAYER_STATISTICS);
                 inventory.setItem(8, KitItems.OPEN_EDITOR_ITEM);
 
                 ItemStack eventItem = EventItems.getEventItem();
