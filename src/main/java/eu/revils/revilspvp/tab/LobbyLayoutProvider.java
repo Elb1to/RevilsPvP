213d441effd9e80974b66b57a5c2380188d1bb7b
Elb1to <elbitomc@gmail.com>
2020-05-11 14:52:03 -0500

    Tablist now sorting ranks correctly

--- src/main/java/eu/revils/revilspvp/tab/LobbyLayoutProvider.java
+++ src/main/java/eu/revils/revilspvp/tab/LobbyLayoutProvider.java
@@ -1,12 +1,16 @@
 package eu.revils.revilspvp.tab;
 
-import me.activated.core.plugin.AquaCoreAPI;
+import me.activated.core.api.player.PlayerData;
+import me.activated.core.api.rank.RankData;
+import me.activated.core.plugin.AquaCore;
 import net.frozenorb.qlib.tab.TabLayout;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiConsumer;
 
 final class LobbyLayoutProvider implements BiConsumer<Player, TabLayout> {
@@ -20,17 +24,21 @@ final class LobbyLayoutProvider implements BiConsumer<Player, TabLayout> {
         tabLayout.set(2, 1, "&7&m----------------");
 
         int x = 0;
-        int y = 0;
+        int y = 2;
 
-        for (final Player online : Bukkit.getServer().getOnlinePlayers()) {
-            ChatColor profile = AquaCoreAPI.INSTANCE.getPlayerData(online.getUniqueId()).getHighestRank().getColor();
-            if (profile != null) {
-                tabLayout.set(x, y, (profile + online.getName()), ((CraftPlayer) online).getHandle().ping);
-                if (x++ < 2) {
-                    continue;
+        for (RankData rank : getSortedRanks()) {
+            for (final Player online : Bukkit.getServer().getOnlinePlayers()) {
+                PlayerData profile = AquaCore.INSTANCE.getPlayerManagement().getPlayerData(online.getUniqueId());
+                if (profile != null) {
+                    if (profile.getHighestRank() == rank) {
+                        tabLayout.set(x, y, (rank.getDisplayColor() + online.getName()), ((CraftPlayer) online).getHandle().ping);
+                        if (x++ < 2) {
+                            continue;
+                        }
+                        x = 0;
+                        ++y;
+                    }
                 }
-                x = 0;
-                ++y;
             }
         }
 
@@ -42,4 +50,11 @@ final class LobbyLayoutProvider implements BiConsumer<Player, TabLayout> {
         tabLayout.set(1, 19, "&7ts.revils.eu");
         tabLayout.set(2, 19, "&7store.revils.eu");
     }
+
+    public List<RankData> getSortedRanks() {
+        List<RankData> ranks = new ArrayList<>(AquaCore.INSTANCE.getRankManagement().getRanks());
+        ranks.sort(Comparator.comparing(RankData::getWeight).reversed());
+        return ranks;
+    }
+
 }
