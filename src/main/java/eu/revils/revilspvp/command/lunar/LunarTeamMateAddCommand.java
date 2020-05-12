0332fe23685d0b6304dd92c60f8d8bb13cc032dc
Elb1to <elbitomc@gmail.com>
2020-05-12 18:06:15 -0500

    Update LunarTeamMateAddCommand.java

--- src/main/java/eu/revils/revilspvp/command/lunar/LunarTeamMateAddCommand.java
+++ src/main/java/eu/revils/revilspvp/command/lunar/LunarTeamMateAddCommand.java
@@ -6,21 +6,23 @@ import net.mineaus.lunar.api.LunarClientAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+
 public class LunarTeamMateAddCommand {
 
     @Command(names = {"lunar_teammate_add"}, permission = "op")
-    public static void lunarTeammateDisplay(Player sender, @Param(name = "target") Player target) {
-
+    public static void lunarTeammateDisplay(Player sender, @Param(name="player1") Player p1, @Param(name="player2") Player p2) {
+        ArrayList<Player> players = new ArrayList<>();
         try {
             if (sender != null) {
-                LunarClientAPI.INSTANCE().sendTeamMate(sender, target);
+                LunarClientAPI.INSTANCE().sendTeamMate(sender, p1, p2);
+                //LunarClientAPI.INSTANCE().sendTeamMate(sender, players.toArray(new Player[players.size()]));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         assert sender != null;
-        sender.sendMessage(ChatColor.GREEN + "You added " + ChatColor.AQUA + target + ChatColor.GREEN + " to your team.");
-        sender.sendMessage(ChatColor.GRAY + ChatColor.ITALIC.toString() + "(testing)");
+        sender.sendMessage(ChatColor.GREEN + "You added " + ChatColor.AQUA + players.size() + ChatColor.GREEN + " to your team.");
     }
 
 }
