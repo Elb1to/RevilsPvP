f5decb5db1e3800ebcb14241706e8a072385b57b
Elb1to <elbitomc@gmail.com>
2020-05-12 16:53:49 -0500

    Create LunarNametagTeamResetCommand.java

---
+++ src/main/java/eu/revils/revilspvp/command/lunar/LunarNametagTeamResetCommand.java
@@ -0,0 +1,25 @@
+package eu.revils.revilspvp.command.lunar;
+
+import eu.revils.revilspvp.kt.command.Command;
+import eu.revils.revilspvp.kt.command.data.parameter.Param;
+import net.mineaus.lunar.api.LunarClientAPI;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class LunarNametagTeamResetCommand {
+
+    @Command(names = {"lunar_reset_nametag"}, permission = "op")
+    public static void resetLunarNametag(Player sender, @Param(name = "target") Player target) {
+
+        try {
+            if (sender != null) {
+                LunarClientAPI.INSTANCE().resetNameTag(sender, target);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assert sender != null;
+        sender.sendMessage(ChatColor.RED + "You resetted the player nametags you added for yourself!");
+        sender.sendMessage(ChatColor.GRAY + ChatColor.ITALIC.toString() + "(testing)");
+    }
+}
