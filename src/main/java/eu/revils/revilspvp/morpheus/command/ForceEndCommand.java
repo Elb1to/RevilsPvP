14f2a3d99fd1b916c1713b945ae915f7f238ce6a
Elb1to <elbitomc@gmail.com>
2020-05-13 23:21:36 -0500

    Update ForceEndCommand.java

--- src/main/java/eu/revils/revilspvp/morpheus/command/ForceEndCommand.java
+++ src/main/java/eu/revils/revilspvp/morpheus/command/ForceEndCommand.java
@@ -3,11 +3,12 @@ package eu.revils.revilspvp.morpheus.command;
 import com.qrakn.morpheus.game.Game;
 import com.qrakn.morpheus.game.GameQueue;
 import eu.revils.revilspvp.kt.command.Command;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class ForceEndCommand {
 
-    @Command(names = {"forceend"}, permission = "op")
+    @Command(names = {"forceend", "event cancel","stopevent", "cancelevent"}, permission = "op")
     public static void host(Player sender) {
         Game game = GameQueue.INSTANCE.getCurrentGame(sender);
 
@@ -16,6 +17,7 @@ public class ForceEndCommand {
             return;
         }
         game.end();
+        sender.sendMessage(ChatColor.GREEN + "Successfully stopped the ongoing event.");
     }
 
 }
